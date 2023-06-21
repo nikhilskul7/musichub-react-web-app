c525c15c0a984c362aa7323da107172e61917682
esha <acharya.e@northeastern.edu>
2023-06-21 16:37:06 -0400

    services folder

--- src/services/song-details-service.js
+++ src/services/song-details-service.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const SEARCH_URL = process.env.REACT_MEAL_DETAILS || "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+const SEARCH_URL = process.env.REACT_SONG_DETAILS || "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
 
-export const mealDetailsService = async (mid) => {
-  const mealdbResponse = await axios.get(`${SEARCH_URL}${mid}`);
-  return mealdbResponse.data.meals[0];
+export const songDetailsService = async (mid) => {
+  const songdbResponse = await axios.get(`${SEARCH_URL}${mid}`);
+  return songdbResponse.data.songs[0];
 };
 
-export const getMealReviewsService = async (mid) => {
+export const getSongReviewsService = async (mid) => {
   const reponse = await axios.get(`$`);
 };
 
-export const postMealCommentService = async (comment) => {};
+export const postSongCommentService = async (comment) => {};

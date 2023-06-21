fa8be3e61aa6dd19055cbca30884529299c19911
esha <acharya.e@northeastern.edu>
2023-06-21 16:22:37 -0400

    random-reciepe folder

--- src/components/random-recipe/getRandomMeal.js
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import fetchRandomMeal from "./fetchRandomMeal";
-
-export const getRandomMeal = createAsyncThunk(
-  "getRandomMeal",
-  async () => await fetchRandomMeal()
-);
-export default getRandomMeal;

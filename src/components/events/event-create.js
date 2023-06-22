d0f437246fce9a1feac2f67f44d581f6c3cdd802
Nikhil Kulkarni <nikhilskul7@gmail.com>
2023-06-22 17:12:07 -0400

    Changed frontend of events

--- src/components/events/event-create.js
+++ src/components/events/event-create.js
@@ -9,6 +9,7 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import { Alert } from "react-bootstrap";
+import "./index.css";
 
 const EventCreate = () => {
   const [title, setTitle] = useState("");
@@ -38,66 +39,65 @@ const EventCreate = () => {
     }
   };
   return (
-    <div>
-      <Link to={"/event"} className={"text-decoration-none text-secondary"}>
-        <i className="bi bi-arrow-left me-1"></i>Back
-      </Link>
-      <h2>Create an Event</h2>
+    <div className="createEventContainer">
+    <Link to="/event" className="backLink">
+      <i className="bi bi-arrow-left me-1"></i>Back
+    </Link>
+    <h2 className="sectionTitle">Create an Event</h2>
 
-      <Alert
-        variant="danger"
-        onClose={() => setTitleAlert(false)}
-        className={titleAlert ? "d-block" : "d-none"}
-        dismissible
-      >
-        <span>Please enter the title of the event!</span>
-      </Alert>
-      <Alert
-        variant="danger"
-        onClose={() => setSummaryAlert(false)}
-        className={summaryAlert ? "d-block" : "d-none"}
-        dismissible
-      >
-        <span>Please enter the description of the event!</span>
-      </Alert>
+    <Alert
+      variant="danger"
+      onClose={() => setTitleAlert(false)}
+      className={titleAlert ? "alertVisible" : "alertHidden"}
+      dismissible
+    >
+      Please enter the title of the event!
+    </Alert>
+    <Alert
+      variant="danger"
+      onClose={() => setSummaryAlert(false)}
+      className={summaryAlert ? "alertVisible" : "alertHidden"}
+      dismissible
+    >
+      Please enter the description of the event!
+    </Alert>
 
-      <Container>
-        <span className={"text-muted"}>Create your event here</span>
-        <Form>
-          <FloatingLabel controlId="eventTitle" label="Title *" className="mb-3">
-            <Form.Control
-              type="text"
-              value={title}
-              placeholder="Title"
-              onChange={(event) => setTitle(event.target.value)}
-            />
-          </FloatingLabel>
-          <FloatingLabel controlId="eventBody" label="Body *">
-            <Form.Control
-              as="textarea"
-              placeholder="Description"
-              style={{ height: "15rem" }}
-              value={summary}
-              onChange={(event) => setSummary(event.target.value)}
-            />
-          </FloatingLabel>
-          <Form.Text id="passwordHelpBlock" muted>
-            Create your event using Markdown.
-          </Form.Text>
-        </Form>
+    <Container>
+      <span className="text-muted">Create your event here</span>
+      <Form>
+        <FloatingLabel controlId="eventTitle" label="Title *" className="mb-3">
+          <Form.Control
+            type="text"
+            value={title}
+            placeholder="Title"
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </FloatingLabel>
+        <FloatingLabel controlId="eventBody" label="Body *">
+          <Form.Control
+            as="textarea"
+            placeholder="Description"
+            style={{ height: "15rem" }}
+            value={summary}
+            onChange={(event) => setSummary(event.target.value)}
+          />
+        </FloatingLabel>
+        <Form.Text id="passwordHelpBlock" muted>
+          Create your event using Markdown.
+        </Form.Text>
+      </Form>
 
-        <hr />
+      <hr />
 
-        <hr className={"d-block d-md-none"} />
-        <span className={"text-muted"}>Preview</span>
-        <h3>{title}</h3>
-        <ReactMarkdown children={summary} />
+      <span className="text-muted">Preview</span>
+      <h3 className="previewTitle">{title}</h3>
+      <ReactMarkdown children={summary} />
 
-        <Button onClick={createEvent} className={"mb-3 mt-5"}>
-          Publish Event
-        </Button>
-      </Container>
-    </div>
+      <Button onClick={createEvent} className="publishButton">
+        Publish Event
+      </Button>
+    </Container>
+  </div>
   );
 };
 
--- src/components/events/index.js
+++ src/components/events/index.js
@@ -4,69 +4,67 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { getAllEventsThunk } from "./event-thunks";
 import { parseTime } from "./parseTime";
-import { Link } from "react-router-dom";
-import { userLikesNotesThunk } from "../likes/likes-thunks";
-import { followUserThunk } from "../follows/follows-thunks";
+
+
 import "../index.css";
 
-const events = [
-  {
-    _id: "123",
-    title: "event1",
-    username: "bob",
-    summary: "this is event 1",
-    date: "2023-12-12",
-  },
-  {
-    _id: "234",
-    title: "event2",
-    username: "alice",
-    summary: "this is event2",
-    date: "2024-04-17",
-  },
-];
 
 const Event = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.users);
   const { event, loading } = useSelector((state) => state.event);
+  const [hoveredEvent, setHoveredEvent] = useState(null);
 
-  const [liked, setLiked] = useState(false);
-  const handleLikeBtn = () => {
-    dispatch(userLikesNotesThunk());
+  const handleEventHover = (eventId) => {
+    setHoveredEvent(eventId);
   };
 
+  const handleEventClick = (eventId) => {
+    navigate("details/" + eventId);
+  };
+  
   useEffect(() => {
     dispatch(getAllEventsThunk());
   }, []);
 
   console.log(event, loading);
   return (
-    <div>
-      {currentUser !== null &&
-        (currentUser.role === "MUSIC-CREATOR" || currentUser.role === "ADMIN") && (
-          <Button onClick={() => navigate("create")}>Create</Button>
-        )}
-      <h2>Recent Event</h2>
-      <ul className={"list-group"}>
-        {event.map((e) => (
-          <li
-            className={"list-group-item wd-cursor-pointer"}
-            onClick={() => navigate("details/" + e._id)}
-            key={e._id}
-          >
-            <h5>{e.title}</h5>
-
-            <div className={"text-secondary"}>
-              <span>By: {e.host.hostName}</span>
-              <i className="bi bi-dot"></i>
-              <span>{parseTime(e.date)}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <div className="eventListContainer">
+   
+    <h2 className="sectionTitle">Recent Events</h2>
+    <ul className="list-group eventList">
+      {event.map((event) => (
+        <li
+          key={event._id}
+          className={
+            "list-group-item wd-cursor-pointer" +
+            (hoveredEvent === event._id ? " active" : "")
+          }
+          onClick={() => handleEventClick(event._id)}
+          onMouseEnter={() => handleEventHover(event._id)}
+          onMouseLeave={() => handleEventHover(null)}
+        >
+          <h5 className="eventTitle">{event.title}</h5>
+          <div className="eventDetails">
+            <span className="hostName">By: {event.host.hostName}</span>
+            <span className="dotSeparator">&#8226;</span>
+            <span className="eventDate">{parseTime(event.date)}</span>
+          </div>
+        </li>
+      ))}
+    </ul>
+    <br></br>
+    
+    <br></br>
+    {currentUser !== null &&
+      (currentUser.role === "MUSIC-CREATOR" ||
+        currentUser.role === "ADMIN") && (
+        <Button className="createButton" onClick={() => navigate("create")}>
+          Create
+        </Button>
+      )}
+  </div>
   );
 };
 export default Event;
